38301794717acb0f9ae71bf63286cdf841ce83f3
jiyunju <ch00251@naver.com>
2020-02-03 15:03:14 +0900

    [Spring05] /users/signup.do 요청 처리함

--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersSerivceImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersSerivceImpl.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gura.spring05.users.dao.UsersDao;
+import com.gura.spring05.users.dto.UsersDto;
 
 @Service
 public class UsersSerivceImpl implements UsersService{
 
 	@Autowired
 	private UsersDao dao;
+	
 	//인자로 전달된 아이디가 존재하는지 여부를 Map 에 담아서 리턴하는 메소드
 	@Override
 	public Map<String, Object> isExistId(String inputId) {
@@ -20,6 +22,10 @@ public class UsersSerivceImpl implements UsersService{
 		Map<String, Object> map=new HashMap<>();//Map에 {"isExist":true} 이런식으로 응답함
 		map.put("isExist", isExist);//isExist라는 key값으로 true,false가 담긴다.
 		return map;
+	}
+	@Override
+	public void addUser(UsersDto dto) {
+		dao.insert(dto);
 	}
 
 }
