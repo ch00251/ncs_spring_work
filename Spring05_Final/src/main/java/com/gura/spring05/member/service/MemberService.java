1540a11fd7c7644854ddca410a627a9d7c866bfb
jiyunju <ch00251@naver.com>
2020-02-03 10:46:32 +0900

    [Spring05] 회원정보 수정,삭제를 MemberServiceImpl 객체를 이용해서 구현함

--- Spring05_Final/src/main/java/com/gura/spring05/member/service/MemberService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/member/service/MemberService.java
@@ -8,5 +8,8 @@ public interface MemberService {
 	//회원 목록 얻어오기 처리 메소드, mView는 dao를 이용해서 회원 목록을 담는 역할을 하면 됨
 	public void getList(ModelAndView mView);
 	public void addMember(MemberDto dto);
+	public void getMember(ModelAndView mView, int num);
+	public void updateMember(MemberDto dto);
+	public void deleteMember(int num);
 	
 }
