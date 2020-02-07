a7d22690fd6b6780368c2e336a7a4cea3806d39a
jiyunju <ch00251@naver.com>
2020-02-07 16:43:40 +0900

    [Spring05] /cafe/comment_insert.do 댓글 추가 기능 구현함

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
@@ -96,4 +96,12 @@ public class CafeController {
 				("redirect:/cafe/detail.do?num="+dto.getNum());
 	}
 	
+	//댓글 저장 요청 처리
+	@RequestMapping(value = "/cafe/comment_insert", method=RequestMethod.POST)
+	public ModelAndView authCommentInsert(HttpServletRequest request, 
+			@RequestParam int ref_group) {
+		service.saveComment(request);
+		return new ModelAndView("redirect:/cafe/detail.do?num="+ref_group); 
+	}
+	
 }
